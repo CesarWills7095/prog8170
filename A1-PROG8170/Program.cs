c0aeefcc18a88286c897125b04516651d3578b13
Cesar Wills <cwillsenamorado7095@conestogac.on.ca>
2021-02-18 19:51:30 -0600

    a1 initial commit 2

---
+++ A1-PROG8170/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1_PROG8170
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ A1-PROG8170/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1_PROG8170
+{
+    class Rectangle
+    {
+        private int _length;
+        private int _width;
+        public Rectangle()
+        {
+            _width = 1;
+            _length = 1;
+        }
+        public Rectangle(int width, int length)
+        {
+            _width = width;
+            _length = length;
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public int GetLength()
+        {
+            return _length;
+        }
+        public int SetLength(int length)
+        {
+            _length = length;
+            return _length;
+        }
+        public int SetWidth(int width)
+        {
+            _width = width;
+            return _width;
+        }
+        public int GetPerimeter()
+        {
+            return (2*_width + 2*_length);
+        }
+        public int GetArea()
+        {
+            return (_width * _length);
+        }
+
+    }
+}
